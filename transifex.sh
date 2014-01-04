0518913a6a9852cf79fea27fe00084e6b70e9f52
Thomas Tanghus <thomas@tanghus.net>
2014-01-04 11:45:51 +0100

    Echo one line by one

--- transifex.sh
+++ transifex.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-echo "[https://www.transifex.com]\nhostname = https://www.transifex.com\npassword = $TRANSIFEX\ntoken = \nusername = tanghus" > ~/.transifexrc
-cat ~/.transifexrc
 cd translations
 
 git clone http://code.transifex.com/transifex-client
 cd transifex-client
 python setup.py install --user
 
+echo "[https://www.transifex.com]" > ~/.transifexrc
+echo "hostname = https://www.transifex.com" >> ~/.transifexrc
+echo "password = $TRANSIFEX" >> ~/.transifexrc
+echo "token = " >> ~/.transifexrc
+echo "username = tanghus" >> ~/.transifexrc
+cat ~/.transifexrc
+
 ~/.local/bin/tx pull --all
+
+rm  ~/.transifexrc
