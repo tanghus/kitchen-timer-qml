0a4696201270a1d12bac5ce7558e0904f2182479
Thomas Tanghus <thomas@tanghus.net>
2014-01-06 00:57:48 +0100

    My shell scripting abilities are very limited...

--- transifex.sh
+++ transifex.sh
@@ -4,22 +4,22 @@ git clone http://code.transifex.com/transifex-client > /dev/null
 cd transifex-client
 python setup.py install --user > /dev/null
 
-cat >> ~/.transifexrc <<EOF
-hostname = https://www.transifex.com
-password = $TRANSIFEX
-token =
-username = tanghus
-EOF
+#cat >> ~/.transifexrc <<EOF
+#hostname = https://www.transifex.com
+#password = $TRANSIFEX
+#token =
+#username = tanghus
+#EOF
 
-#echo "[https://www.transifex.com]" > ~/.transifexrc
-#echo "hostname = https://www.transifex.com" >> ~/.transifexrc
-#echo "password = $TRANSIFEX" >> ~/.transifexrc
-#echo "token = " >> ~/.transifexrc
-#echo "username = tanghus" >> ~/.transifexrc
+echo "[https://www.transifex.com]" > ~/.transifexrc
+echo "hostname = https://www.transifex.com" >> ~/.transifexrc
+echo "password = $TRANSIFEX" >> ~/.transifexrc
+echo "token = " >> ~/.transifexrc
+echo "username = tanghus" >> ~/.transifexrc
 
 cd ../translations
 
-~/.local/bin/tx push -s en_GB.ts
-~/.local/bin/tx pull --all
+~/.local/bin/tx push -s en_GB.ts || exit 1
+~/.local/bin/tx pull --all || exit 1
 
 rm  ~/.transifexrc
