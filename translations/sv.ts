5c1ce4d94d5ddbf8ef5b734d4733ece49dfb9ee1
Thomas Tanghus <thomas@tanghus.net>
2014-01-05 04:59:34 +0100

    Updated from Transifex

---
+++ translations/sv.ts
@@ -0,0 +1,118 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sv" version="2.0">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="45"/>
+        <source>Simple Kitchen Timer app</source>
+        <translation>En enkel äggklocka-app</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="47"/>
+        <source>by %1</source>
+        <comment>As in made by %1</comment>
+        <extracomment>Naming the author</extracomment>
+        <translation>av %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="49"/>
+        <source>See more at %1.</source>
+        <comment>Link URL</comment>
+        <extracomment>Link to Github project page</extracomment>
+        <translation>Läs mer på %1.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="49"/>
+        <source>the project page</source>
+        <comment>Link text</comment>
+        <extracomment>Link to Github project page</extracomment>
+        <translation>projektsidan</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="51"/>
+        <source>Issues and feature request at the %1</source>
+        <comment>Link URL</comment>
+        <extracomment>Link to the issue tracker</extracomment>
+        <translation>Frågor och önskemål om nya funktioner på %1 </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="51"/>
+        <source>issue tracker</source>
+        <comment>Link text</comment>
+        <extracomment>Link to the issue tracker</extracomment>
+        <translation>ärendehanteringssystem</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="53"/>
+        <source>Ask &quot;%1&quot; at the %2 channel on Freenode IRC for support</source>
+        <comment>Link with text</comment>
+        <extracomment>%1: Author nick, %2:Link to Web IRC</extracomment>
+        <translation>Fråga &quot;%1&quot; på kanalen %2 på Freenode IRC för support</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="54"/>
+        <source>The awesome icon is made by %1</source>
+        <translation>Den coola ikonen är gjord av %1</translation>
+    </message>
+</context>
+<context>
+    <name>TimerPage</name>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="86"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="92"/>
+        <source>Edit default timers</source>
+        <translation>Redigera default-timers</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="121"/>
+        <source>Kitchen Timer</source>
+        <translation>Äggklocka</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="249"/>
+        <source>Start</source>
+        <translation>Starta</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="250"/>
+        <source>Pause</source>
+        <translation>Pausa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="251"/>
+        <source>Reset</source>
+        <translation>Återställ</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="252"/>
+        <source>Mute</source>
+        <translation>Ljudlös</translation>
+    </message>
+</context>
+<context>
+    <name>TimersDialog</name>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="56"/>
+        <source>Deleting</source>
+        <translation>Tar bort</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="65"/>
+        <source>Timer name</source>
+        <translation>Timernamn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="78"/>
+        <source>Minutes</source>
+        <translation>Minuter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="103"/>
+        <source>Seconds</source>
+        <translation>Sekunder</translation>
+    </message>
+</context>
+</TS>
