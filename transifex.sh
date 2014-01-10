d933bf816e8cabcb2da4dd32cf70b8b9c44f7c09
Thomas Tanghus <thomas@tanghus.net>
2014-01-10 05:13:13 +0100

    Cleanup 2

--- transifex.sh
+++ transifex.sh
@@ -4,25 +4,11 @@ git clone http://code.transifex.com/transifex-client > /dev/null
 cd transifex-client
 python setup.py install --user > /dev/null
 
-cat >> ~/.transifexrc <<EOF
-hostname = https://www.transifex.com
-password = $TRANSIFEX
-token =
-username = tanghus
-EOF
-
-#echo "[https://www.transifex.com]" > ~/.transifexrc
-#echo "hostname = https://www.transifex.com" >> ~/.transifexrc
-#echo "password = $TRANSIFEX" >> ~/.transifexrc
-#echo "token = " >> ~/.transifexrc
-#echo "username = tanghus" >> ~/.transifexrc
-
-cat >> ~/.transifexrc <<EOF
-hostname = https://www.transifex.com
-password = $TRANSIFEX
-token =
-username = tanghus
-EOF
+echo "[https://www.transifex.com]" > ~/.transifexrc
+echo "hostname = https://www.transifex.com" >> ~/.transifexrc
+echo "password = $TRANSIFEX" >> ~/.transifexrc
+echo "token = " >> ~/.transifexrc
+echo "username = tanghus" >> ~/.transifexrc
 
 cd ../translations
 
