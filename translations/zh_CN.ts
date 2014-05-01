272092a6400be45d7e98ded20a68cca52daed0d2
Thomas Tanghus <thomas@tanghus.net>
2014-05-01 04:30:10 +0200

    Updated from Transifex

---
+++ translations/zh_CN.ts
@@ -0,0 +1,118 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.0">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="45"/>
+        <source>Simple Kitchen Timer app</source>
+        <translation>简洁的厨房定时器程序</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="47"/>
+        <source>by %1</source>
+        <comment>As in made by %1</comment>
+        <extracomment>Naming the author</extracomment>
+        <translation>由%1开发</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="49"/>
+        <source>See more at %1.</source>
+        <comment>Link URL</comment>
+        <extracomment>Link to Github project page</extracomment>
+        <translation>到%1了解更多。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="49"/>
+        <source>the project page</source>
+        <comment>Link text</comment>
+        <extracomment>Link to Github project page</extracomment>
+        <translation>项目主页</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="51"/>
+        <source>Issues and feature requests at the %1</source>
+        <comment>Link URL</comment>
+        <extracomment>Link to the issue tracker</extracomment>
+        <translation>报告问题或请求新功能请到%1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="51"/>
+        <source>issue tracker</source>
+        <comment>Link text</comment>
+        <extracomment>Link to the issue tracker</extracomment>
+        <translation>问题跟踪</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="53"/>
+        <source>Ask &quot;%1&quot; at the %2 channel on Freenode IRC for support</source>
+        <comment>Link with text</comment>
+        <extracomment>%1: Author nick, %2:Link to Web IRC</extracomment>
+        <translation>在 Freenode 的 IRC %2 频道向“%1”寻求帮助</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="54"/>
+        <source>The awesome icon is made by %1</source>
+        <translation>图标由%1制作</translation>
+    </message>
+</context>
+<context>
+    <name>TimerPage</name>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="60"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="66"/>
+        <source>Edit default timers</source>
+        <translation>编辑默认定时</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="95"/>
+        <source>Kitchen Timer</source>
+        <translation>厨房定时器</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="186"/>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="187"/>
+        <source>Pause</source>
+        <translation>暂停</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="188"/>
+        <source>Reset</source>
+        <translation>重置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="189"/>
+        <source>Mute</source>
+        <translation>静音</translation>
+    </message>
+</context>
+<context>
+    <name>TimersDialog</name>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="56"/>
+        <source>Deleting</source>
+        <translation>正在删除</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="65"/>
+        <source>Timer name</source>
+        <translation>定时名称</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="78"/>
+        <source>Minutes</source>
+        <translation>分钟</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="103"/>
+        <source>Seconds</source>
+        <translation>秒</translation>
+    </message>
+</context>
+</TS>
