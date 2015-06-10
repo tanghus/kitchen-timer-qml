d0e104f4048f0d6964176c3379729c53ea9d325d
Thomas Tanghus <thomas@tanghus.net>
2015-06-10 14:32:04 +0200

    Added new translations

---
+++ translations/es.ts
@@ -0,0 +1,207 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="46"/>
+        <source>Simple Kitchen Timer app</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="48"/>
+        <source>by %1</source>
+        <comment>As in made by %1</comment>
+        <extracomment>Naming the author</extracomment>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="50"/>
+        <source>See more at %1.</source>
+        <comment>Link URL</comment>
+        <extracomment>Link to Github project page</extracomment>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="50"/>
+        <source>the project page</source>
+        <comment>Link text</comment>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="52"/>
+        <source>Issues and feature requests at the %1</source>
+        <comment>Link URL</comment>
+        <extracomment>Link to the issue tracker</extracomment>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="52"/>
+        <source>issue tracker</source>
+        <comment>Link text</comment>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="54"/>
+        <source>Ask &quot;%1&quot; at the %2 channel on Freenode IRC for support</source>
+        <comment>Link with text</comment>
+        <extracomment>%1: Author nick, %2:Link to Web IRC</extracomment>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="55"/>
+        <source>The awesome icon is made by %1</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>SoundDialog</name>
+    <message>
+        <location filename="../qml/pages/SoundDialog.qml" line="46"/>
+        <source>Sound</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundDialog.qml" line="67"/>
+        <source>Disable sound</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundDialog.qml" line="82"/>
+        <source>Vibrate</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundDialog.qml" line="96"/>
+        <source>Select music file</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>SoundSelectDialog</name>
+    <message>
+        <location filename="../qml/pages/SoundSelectDialog.qml" line="44"/>
+        <source>Select</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundSelectDialog.qml" line="45"/>
+        <source>Select sound file</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundSelectDialog.qml" line="59"/>
+        <source>Up</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundSelectDialog.qml" line="152"/>
+        <source>No sound files here.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>SoundsDialog</name>
+    <message>
+        <source>Deleting</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>Storage</name>
+    <message>
+        <location filename="../qml/components/Storage.qml" line="156"/>
+        <source>Eggs</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/components/Storage.qml" line="157"/>
+        <source>Potatoes</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/components/Storage.qml" line="158"/>
+        <source>Frozen pizza</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/components/Storage.qml" line="159"/>
+        <source>Tea (Earl Grey)</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>TimerPage</name>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="62"/>
+        <source>About</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="68"/>
+        <source>Edit default timers</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="72"/>
+        <source>Set alarm sound</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="77"/>
+        <source>Select sound</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="81"/>
+        <source>Last timer:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="118"/>
+        <source>Kitchen Timer</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="209"/>
+        <source>Start</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="210"/>
+        <source>Pause</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="211"/>
+        <source>Reset</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="212"/>
+        <source>Mute</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>TimersDialog</name>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="58"/>
+        <source>Deleting</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="67"/>
+        <source>Timer name</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="80"/>
+        <source>Minutes</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="105"/>
+        <source>Seconds</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
---
+++ translations/zh_CN.ts
@@ -0,0 +1,207 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="46"/>
+        <source>Simple Kitchen Timer app</source>
+        <translation>简洁的厨房定时器程序</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="48"/>
+        <source>by %1</source>
+        <comment>As in made by %1</comment>
+        <extracomment>Naming the author</extracomment>
+        <translation>由%1开发</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="50"/>
+        <source>See more at %1.</source>
+        <comment>Link URL</comment>
+        <extracomment>Link to Github project page</extracomment>
+        <translation>到%1了解更多。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="50"/>
+        <source>the project page</source>
+        <comment>Link text</comment>
+        <translation>项目主页</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="52"/>
+        <source>Issues and feature requests at the %1</source>
+        <comment>Link URL</comment>
+        <extracomment>Link to the issue tracker</extracomment>
+        <translation>报告问题或请求新功能请到%1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="52"/>
+        <source>issue tracker</source>
+        <comment>Link text</comment>
+        <translation>问题跟踪</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="54"/>
+        <source>Ask &quot;%1&quot; at the %2 channel on Freenode IRC for support</source>
+        <comment>Link with text</comment>
+        <extracomment>%1: Author nick, %2:Link to Web IRC</extracomment>
+        <translation>在 Freenode 的 IRC %2 频道向“%1”寻求帮助</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="55"/>
+        <source>The awesome icon is made by %1</source>
+        <translation>图标由%1制作</translation>
+    </message>
+</context>
+<context>
+    <name>SoundDialog</name>
+    <message>
+        <location filename="../qml/pages/SoundDialog.qml" line="46"/>
+        <source>Sound</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundDialog.qml" line="67"/>
+        <source>Disable sound</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundDialog.qml" line="82"/>
+        <source>Vibrate</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundDialog.qml" line="96"/>
+        <source>Select music file</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>SoundSelectDialog</name>
+    <message>
+        <location filename="../qml/pages/SoundSelectDialog.qml" line="44"/>
+        <source>Select</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundSelectDialog.qml" line="45"/>
+        <source>Select sound file</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundSelectDialog.qml" line="59"/>
+        <source>Up</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SoundSelectDialog.qml" line="152"/>
+        <source>No sound files here.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>SoundsDialog</name>
+    <message>
+        <source>Deleting</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>Storage</name>
+    <message>
+        <location filename="../qml/components/Storage.qml" line="156"/>
+        <source>Eggs</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/components/Storage.qml" line="157"/>
+        <source>Potatoes</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/components/Storage.qml" line="158"/>
+        <source>Frozen pizza</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/components/Storage.qml" line="159"/>
+        <source>Tea (Earl Grey)</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>TimerPage</name>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="62"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="68"/>
+        <source>Edit default timers</source>
+        <translation>编辑默认定时</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="72"/>
+        <source>Set alarm sound</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="77"/>
+        <source>Select sound</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="81"/>
+        <source>Last timer:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="118"/>
+        <source>Kitchen Timer</source>
+        <translation>厨房定时器</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="209"/>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="210"/>
+        <source>Pause</source>
+        <translation>暂停</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="211"/>
+        <source>Reset</source>
+        <translation>重置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimerPage.qml" line="212"/>
+        <source>Mute</source>
+        <translation>静音</translation>
+    </message>
+</context>
+<context>
+    <name>TimersDialog</name>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="58"/>
+        <source>Deleting</source>
+        <translation>正在删除</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="67"/>
+        <source>Timer name</source>
+        <translation>定时名称</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="80"/>
+        <source>Minutes</source>
+        <translation>分钟</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TimersDialog.qml" line="105"/>
+        <source>Seconds</source>
+        <translation>秒</translation>
+    </message>
+</context>
+</TS>
