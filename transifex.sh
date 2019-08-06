d80381ed639c420fba5051744704a806ae22940c
Thomas Tanghus <thomas@tanghus.net>
2019-08-06 13:52:56 +0200

    Needed for translations

--- transifex.sh
+++ transifex.sh
@@ -3,11 +3,12 @@
 git clone http://code.transifex.com/transifex-client > /dev/null || exit 1
 cd transifex-client || exit 1
 python setup.py install --user > /dev/null
+cd ..
 
 echo "[https://www.transifex.com]" > ~/.transifexrc
 echo "hostname = https://www.transifex.com" >> ~/.transifexrc
-echo "password = $TRANSIFEX" >> ~/.transifexrc
-echo "token = " >> ~/.transifexrc
+echo "password = 1/2086ec1d18862fe6f7e630064ae68590372f5c3d" >> ~/.transifexrc
+echo "token = 1/2086ec1d18862fe6f7e630064ae68590372f5c3d" >> ~/.transifexrc
 echo "username = tanghus" >> ~/.transifexrc
 echo "lang_map = el: el_GR, de: de_DE, en: en_GB, da: da_DK, sv: sv_SE, ru: ru_RU" >> ~/.transifexrc
 #echo """ >> ~/.transifexrc
@@ -18,11 +19,11 @@ echo "source_file = en_GB.ts" >> ~/.transifexrc
 echo "source_lang = en_GB" >> ~/.transifexrc
 echo "type = QT" >> ~/.transifexrc
 
-#cat  ~/.transifexrc
+cat  ~/.transifexrc
 
-cd ../translations
+cd translations
 
-~/.local/bin/tx push -s en_GB.ts || exit 1
+~/.local/bin/tx push -s || exit 1
 #~/.local/bin/tx pull -af || exit 1
 
 rm  ~/.transifexrc
